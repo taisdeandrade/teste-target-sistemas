6857470b560d050bb8bd3d5195a5547874f55ae5
TaisAndrade <taismdeandrade@gmail.com>
2024-09-04 14:23:55 -0300

    Update: Exercicio 3 feito

---
+++ faturamento/faturamento/src/main/java/com/tais/Dados.java
@@ -0,0 +1,20 @@
+package com.tais;
+public class Dados {
+    int dia;
+    double valor;
+
+    public int getDia() {
+        return dia;
+    }
+
+    public void setDia(int dia) {
+        this.dia = dia;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+}
